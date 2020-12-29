259abc841dcdb5ab9cb133daf61bec99487aea19
Jaydon Fernandes <60637128+JaydonFernandes@users.noreply.github.com>
2020-12-28 22:52:44 -0700

    fixed clock text size

--- src/components/VisualTimer.js
+++ src/components/VisualTimer.js
@@ -8,7 +8,7 @@ function VisualTimer(props) {
     var seconds = timeInSeconds - (minutes*60);
 
     return (
-        <div className="VisualTimer" style={{margin:"1rem", fontSize: "1000%"}}>
+        <div className="VisualTimer" style={{margin:"1rem", fontSize: "700%"}}>
             {minutes}:<span>{zeroPad(seconds, 2)}</span>
         </div>
     );
