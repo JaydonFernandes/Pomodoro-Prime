4da5f69e8ac0ca31e5f5d6eadb21cc644a285821
Jaydon Fernandes <60637128+JaydonFernandes@users.noreply.github.com>
2020-12-30 01:13:52 -0700

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -5,14 +5,7 @@ const self = this;
 
 // Install SW
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-
-                return cache.addAll(urlsToCache);
-            })
-    )
+    
 });
 
 // Listen for requests
